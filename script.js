71b833dea1be958ce262884b3c7705068ff668fe
Ashish Katiwal <142999905+AshishKatiwal@users.noreply.github.com>
2024-03-01 19:13:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function dark(){
+    document.querySelector("body").style.backgroundColor = "rgb(18, 18, 62)";
+}
+
+function light(){
+    document.querySelector("body").style.backgroundColor = "white";
+}
